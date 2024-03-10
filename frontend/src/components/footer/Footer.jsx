69252471eb35844d7a65cc15719795b2bf24e796
AlonaRahilevych <ragilevic@gmail.com>
2024-03-10 14:51:09 +0100

    Add footer component

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,4 +1,28 @@
+import {
+  FacebookLogo,
+  GithubLogo,
+  InstagramLogo,
+  LinkedinLogo,
+} from '@phosphor-icons/react';
+import './Footer.scss';
 const Footer = () => {
-  return <div></div>;
+  return (
+    <div className='footer'>
+      <div className='footer__social'>
+        <div className='footer__circle'>
+          <LinkedinLogo className='footer__icon' size={32} />
+        </div>
+        <div className='footer__circle'>
+          <InstagramLogo className='footer__icon' size={32} />
+        </div>
+        <div className='footer__circle'>
+          <FacebookLogo className='footer__icon' size={32} />
+        </div>
+        <div className='footer__circle'>
+          <GithubLogo className='footer__icon' size={32} />
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Footer;
